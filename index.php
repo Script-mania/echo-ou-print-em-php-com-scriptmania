5556c3ff1d3696b08f0519528155c711555841d4
Script-Mania <fabriciogdo@hotmail.com>
2016-01-23 17:40:17 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+Muita gente tem duvidas de como usar  o echo em php, vou dar alguns exemplos
+<?php echo 'Script-Mania'; ?>
+ou
+<?php print'Script-Mania';?>
+ou a inda
+<?='Script-Manbria';?>
+Qualquer duvida podem comentar.
+http://scriptmania.com.
